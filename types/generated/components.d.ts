7e386f4ebdcd34e684f806d732993961fe6e68fb
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-08-04 18:04:09 +0500

    feat(Global): add Global single type

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,34 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentsMenuItem extends Schema.Component {
+  collectionName: 'components_components_menu_items';
+  info: {
+    displayName: 'Menu Item';
+  };
+  attributes: {
+    href: Attribute.String;
+    icon: Attribute.String;
+    text: Attribute.String;
+  };
+}
+
+export interface LayoutSideNavigation extends Schema.Component {
+  collectionName: 'components_layout_side_navigations';
+  info: {
+    displayName: 'Side Navigation';
+    description: '';
+  };
+  attributes: {
+    logo: Attribute.Media<'images'>;
+    menuItem: Attribute.Component<'components.menu-item', true>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'components.menu-item': ComponentsMenuItem;
+      'layout.side-navigation': LayoutSideNavigation;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,39 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiGlobalGlobal extends Schema.SingleType {
+  collectionName: 'globals';
+  info: {
+    singularName: 'global';
+    pluralName: 'globals';
+    displayName: 'Global';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    sideNavigation: Attribute.Component<'layout.side-navigation'>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::global.global',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::global.global',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -798,6 +831,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::global.global': ApiGlobalGlobal;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
