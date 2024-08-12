603e5a00beb95a83aba16fab6cc7666a69b96c5c
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-08-12 15:50:12 +0500

    feat: add Hero Section component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,27 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface LayoutSideNavigation extends Schema.Component {
-  collectionName: 'components_layout_side_navigations';
-  info: {
-    displayName: 'Side Navigation';
-    description: '';
-  };
-  attributes: {
-    logo: Attribute.Media<'images'>;
-    menuItem: Attribute.Component<'components.menu-item', true>;
-  };
-}
-
-export interface LayoutHeader extends Schema.Component {
-  collectionName: 'components_layout_headers';
-  info: {
-    displayName: 'Header';
-  };
-  attributes: {
-    link: Attribute.Component<'components.link'>;
-  };
-}
-
 export interface ComponentsMenuItem extends Schema.Component {
   collectionName: 'components_components_menu_items';
   info: {
@@ -46,13 +24,49 @@ export interface ComponentsLink extends Schema.Component {
   };
 }
 
+export interface LayoutSideNavigation extends Schema.Component {
+  collectionName: 'components_layout_side_navigations';
+  info: {
+    displayName: 'Side Navigation';
+    description: '';
+  };
+  attributes: {
+    logo: Attribute.Media<'images'>;
+    menuItem: Attribute.Component<'components.menu-item', true>;
+  };
+}
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    image: Attribute.Media<'images'>;
+    headingText: Attribute.String;
+    highlightedText: Attribute.String;
+    subheadingText: Attribute.Text;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'layout.side-navigation': LayoutSideNavigation;
-      'layout.header': LayoutHeader;
       'components.menu-item': ComponentsMenuItem;
       'components.link': ComponentsLink;
+      'layout.side-navigation': LayoutSideNavigation;
+      'layout.hero-section': LayoutHeroSection;
+      'layout.header': LayoutHeader;
     }
   }
 }
